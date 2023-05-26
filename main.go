a9aecee8b6b059c2c825ea4e4653301839f4c7dc
arewabolu <bolu.arewa@gmail.com>
2023-05-26 15:56:47 +0100

    a basic website

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func DefaultHandle(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
 		temp := template.Must(template.ParseFiles("./frontend/index.html"))
 		data := info{
-			Name:   "Hi, I am Arewa Bouwatife",
+			Name:   "Arewa Bolu",
 			Email:  "bolu.arewa@gmail.com",
 			Github: "arewabolu",
 		}
@@ -29,6 +29,8 @@ func DefaultHandle(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", DefaultHandle)
+	styles := http.FileServer(http.Dir("./frontend/style"))
+	http.Handle("/style/", http.StripPrefix("/style/", styles))
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		panic(err)
